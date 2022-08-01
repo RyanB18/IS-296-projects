844b7b12881dde2b27ade150028c492b17574096
RyanB18 <106997786+RyanB18@users.noreply.github.com>
2022-08-01 09:16:02 -0500

    Add files via upload

---
+++ tictactacom/src/main/java/yatesm/tictactacom/TictactacomApplication.java
@@ -0,0 +1,13 @@
+package yatesm.tictactacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactacomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactacomApplication.class, args);
+	}
+
+}
